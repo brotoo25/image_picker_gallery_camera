1bf2a013656e1c3da33f6a2228976e962fd1ece6
Abraão <abraao.lima@tqi.com.br>
2020-11-05 18:23:53 -0300

    Include remove option

--- lib/image_picker_gallery_camera.dart
+++ lib/image_picker_gallery_camera.dart
@@ -7,17 +7,18 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImagePickerGC {
-  static Future pickImage(
-      {@required BuildContext context,
-      @required ImgSource source,
-      double maxWidth,
-      double maxHeight,
-      Icon cameraIcon,
-      Icon galleryIcon,
-      Widget cameraText,
-      Widget galleryText,
-      bool barrierDismissible = false,
-      int imageQuality}) async {
+  static Future pickImage({
+    @required BuildContext context,
+    @required ImgSource source,
+    double maxWidth,
+    double maxHeight,
+    Icon cameraIcon,
+    Icon galleryIcon,
+    Widget cameraText,
+    Widget galleryText,
+    bool barrierDismissible = false,
+    int imageQuality,
+  }) async {
     assert(source != null);
     assert(imageQuality == null || (imageQuality >= 0 && imageQuality <= 100));
 
@@ -29,15 +30,17 @@ class ImagePickerGC {
       throw ArgumentError.value(maxHeight, 'maxHeight cannot be negative');
     }
 
+    final _picker = ImagePicker();
+
     switch (source) {
       case ImgSource.Camera:
-        return await ImagePicker.pickImage(
+        return await _picker.getImage(
             source: ImageSource.camera,
             maxWidth: maxWidth,
             maxHeight: maxHeight);
         break;
       case ImgSource.Gallery:
-        return await ImagePicker.pickImage(
+        return await _picker.getImage(
             source: ImageSource.gallery,
             maxWidth: maxWidth,
             maxHeight: maxHeight);
@@ -56,7 +59,8 @@ class ImagePickerGC {
                 children: <Widget>[
                   InkWell(
                     onTap: () async {
-                      ImagePicker.pickImage(
+                      _picker
+                          .getImage(
                               source: ImageSource.gallery,
                               maxWidth: maxWidth,
                               maxHeight: maxHeight,
@@ -83,7 +87,8 @@ class ImagePickerGC {
                   ),
                   InkWell(
                     onTap: () async {
-                      ImagePicker.pickImage(
+                      _picker
+                          .getImage(
                               source: ImageSource.camera,
                               maxWidth: maxWidth,
                               maxHeight: maxHeight)
@@ -107,8 +112,94 @@ class ImagePickerGC {
             );
           },
         );
+      case ImgSource.Remove:
+        return await showDialog<void>(
+          context: context,
+          barrierDismissible: barrierDismissible, // user must tap button!
+          builder: (BuildContext context) {
+            return AlertDialog(
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(6))),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  InkWell(
+                    onTap: () async {
+                      _picker
+                          .getImage(
+                              source: ImageSource.gallery,
+                              maxWidth: maxWidth,
+                              maxHeight: maxHeight,
+                              imageQuality: imageQuality)
+                          .then((image) {
+                        Navigator.pop(context, image);
+                      });
+                    },
+                    child: Container(
+                      child: ListTile(
+                          title: galleryText ?? Text("Gallery"),
+                          leading: galleryIcon != null
+                              ? galleryIcon
+                              : Icon(
+                                  Icons.image,
+                                  color: Colors.deepPurple,
+                                )),
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 1,
+                    color: Colors.black12,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      _picker
+                          .getImage(
+                              source: ImageSource.camera,
+                              maxWidth: maxWidth,
+                              maxHeight: maxHeight)
+                          .then((image) {
+                        Navigator.pop(context, image);
+                      });
+                    },
+                    child: Container(
+                      child: ListTile(
+                          title: cameraText ?? Text("Camera"),
+                          leading: cameraIcon != null
+                              ? cameraIcon
+                              : Icon(
+                                  Icons.camera,
+                                  color: Colors.deepPurple,
+                                )),
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 1,
+                    color: Colors.black12,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      Navigator.pop(context, null);
+                    },
+                    child: Container(
+                      child: ListTile(
+                        title: cameraText ?? Text("Remove Image"),
+                        leading: Icon(
+                          Icons.remove_circle_outline,
+                          color: Colors.red[300],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
     }
   }
 }
 
-enum ImgSource { Camera, Gallery, Both }
+enum ImgSource { Camera, Gallery, Both, Remove }
